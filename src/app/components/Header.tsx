453f651eb9739f5779847a25f63d8a865652343d
Tomasz Szewczyk <brutusek952@gmail.com>
2025-01-14 22:53:49 +0100

    ER-3 Responsivenes

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,13 @@
 'use client';
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
+import {motion, AnimatePresence} from "framer-motion";
 import SocialLinks from "@/app/components/SocialLinks";
 
 const Header: React.FC = () => {
     const [activeSection, setActiveSection] = useState<string>("");
+    const [titleIndex, setTitleIndex] = useState<number>(0);
+
+    const titles = ["Front End", "Software"];
 
     useEffect(() => {
         const handleScroll = () => {
@@ -27,12 +31,35 @@ const Header: React.FC = () => {
         };
     }, []);
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTitleIndex((prevIndex) => (prevIndex + 1) % titles.length);
+        }, 3000); // Zmiana co 3 sekundy
+
+        return () => clearInterval(interval);
+    }, [titles.length]);
+
     return (
         <header className="bg-primary text-textLight py-8 px-6 font-inter ml-20 lg:ml-0 lg:static sticky top-0">
             <h1 className="text-4xl md:text-6xl font-bold text-oldMoneyGreen">
                 <a href="#about">Tomasz Szewczyk</a>
             </h1>
-            <h2 className="text-2xl text-textLight mt-4">Front End Engineer</h2>
+
+            <div className="h-12 overflow-hidden mt-4">
+                <AnimatePresence mode="wait">
+                    <motion.h2
+                        key={titles[titleIndex]}
+                        initial={{opacity: 0, y: -50}} // Start z góry
+                        animate={{opacity: 1, y: 0}} // Widoczne
+                        exit={{opacity: 0, y: 50}} // Znika na dół
+                        transition={{duration: 0.7, ease: "easeInOut"}}
+                        className="text-2xl text-textLight font-semibold"
+                    >
+                        {titles[titleIndex]} Engineer
+                    </motion.h2>
+                </AnimatePresence>
+            </div>
+
             <p className="mt-4 text-textLight">
                 I am building accessible, pixel-perfect experiences.
             </p>
@@ -40,10 +67,10 @@ const Header: React.FC = () => {
             <nav className="mt-8">
                 <ul className="flex flex-col space-y-6">
                     {[
-                        { id: "about", label: "About" },
-                        { id: "education", label: "Education" },
-                        { id: "experience", label: "Experience" },
-                        { id: "projects", label: "Projects" },
+                        {id: "about", label: "About"},
+                        {id: "education", label: "Education"},
+                        {id: "experience", label: "Experience"},
+                        {id: "projects", label: "Projects"},
                     ].map((item) => (
                         <li
                             key={item.id}
@@ -69,7 +96,7 @@ const Header: React.FC = () => {
                         </li>
                     ))}
                 </ul>
-                <SocialLinks />
+                <SocialLinks/>
             </nav>
         </header>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import {motion} from "framer-motion";
 import Header from "./components/Header";
 import About from "./components/About";
 import Experience from "./components/Experience";
@@ -7,32 +8,82 @@ import Education from "@/app/components/Education";
 import Projects from "@/app/components/Projects";
 
 const Home: React.FC = () => {
+    const sectionVariants = {
+        hidden: {opacity: 0, y: 50},
+        visible: {opacity: 1, y: 0},
+    };
+
     return (
-        <div className="bg-primary text-textLight min-h-screen font-inter text-white bg-black flex flex-col md:flex-row">
+        <div
+            className="bg-primary text-textLight min-h-screen font-inter text-white bg-black flex flex-col md:flex-row">
+            {/* Header for larger screens */}
             <aside className="w-full md:w-1/4 bg-secondary py-8 px-6 h-auto md:h-screen sticky top-0 hidden lg:block">
-                <Header />
+                <Header/>
             </aside>
 
             <main className="flex-1 py-8 px-6 md:px-12 space-y-12">
-                <div className="block  lg:hidden static text-center">
-                    <h1 className="text-6xl font-bold text-oldMoneyGreen mb-0">Tomasz Szewczyk</h1>
+
+                <div className="block lg:hidden static text-center">
+                    <h1 className="text-6xl font-bold text-oldMoneyGreen mb-0">
+                        Tomasz Szewczyk
+                    </h1>
                 </div>
 
-                <section id="about">
-                    <About />
-                </section>
-                <section id="education">
-                    <Education />
-                </section>
-                <section id="experience">
-                    <Experience />
-                </section>
-                <section id="projects">
-                    <Projects />
-                </section>
-                <section id="footer">
-                    <Footer />
-                </section>
+
+                <motion.section
+                    id="about"
+                    variants={sectionVariants}
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{once: true, amount: 0.2}}
+                    transition={{duration: 0.7, ease: "easeInOut"}}
+                >
+                    <About/>
+                </motion.section>
+
+                <motion.section
+                    id="education"
+                    variants={sectionVariants}
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{once: true, amount: 0.2}}
+                    transition={{duration: 0.7, delay: 0.2, ease: "easeInOut"}}
+                >
+                    <Education/>
+                </motion.section>
+
+                <motion.section
+                    id="experience"
+                    variants={sectionVariants}
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{once: true, amount: 0.2}}
+                    transition={{duration: 0.7, delay: 0.4, ease: "easeInOut"}}
+                >
+                    <Experience/>
+                </motion.section>
+
+                <motion.section
+                    id="projects"
+                    variants={sectionVariants}
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{once: true, amount: 0.2}}
+                    transition={{duration: 0.7, delay: 0.6, ease: "easeInOut"}}
+                >
+                    <Projects/>
+                </motion.section>
+
+                <motion.section
+                    id="footer"
+                    variants={sectionVariants}
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{once: true, amount: 0.2}}
+                    transition={{duration: 0.7, delay: 0.8, ease: "easeInOut"}}
+                >
+                    <Footer/>
+                </motion.section>
             </main>
         </div>
     );
